e3a06ccebcc0fd562ffc307e3c9debdb10da10db
saurabh13209 <agrawal99saurabh@gmail.com>
2020-05-22 23:37:14 +0530

    Image animation

--- src/Screens/Stream.js
+++ src/Screens/Stream.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { View, Text, AppState, BackHandler, Image } from 'react-native';
+import { View, Text, AppState, BackHandler, Image, Animated } from 'react-native';
 import Video from 'react-native-video';
 import YouTube from 'react-native-youtube';
 import io from 'socket.io-client';
@@ -15,6 +15,7 @@ export default Stream = (props) => {
         width: 400
     });
     this.videoStatus = "stopped";
+    const bottomValue = useRef(new Animated.Value(0)).current;
 
     removeUserFunc = () => {
         getData(user).then(res => {
@@ -52,7 +53,15 @@ export default Stream = (props) => {
         return true
     })
     useEffect(() => {
+        // Animated.timing(
+        //     bottomValue,
+        //     {
+        //         toValue: dime.height + 50,
+        //         duration: 4000
+        //     }
+        // ).start()
         this.socket = io(baseUrl);
+        this.isFullScreen = false;
         getData(user).then(username => {
             this.socket.on("connect", () => {
                 this.socket.emit("updateUser", {
@@ -83,6 +92,8 @@ export default Stream = (props) => {
                         })
                     }
                 }
+                console.log(this.isFullScreen)
+
             }, 500)
 
 
@@ -100,7 +111,6 @@ export default Stream = (props) => {
                     }
 
                 })
-
             })
 
             this.socket.on("closeVideo", res => {
@@ -112,35 +122,49 @@ export default Stream = (props) => {
 
     return (
         <View style={{ flex: 1 }}>
-            <YouTube
-                style={{ zIndex: 0 }}
-                ref={refVideo}
-                apiKey="AIzaSyCm7cvQdOwCnslbRqECA015md9Pj_n4ZnM"
-                videoId={props.route.params.data["videoUrl"]}
-                style={{ alignSelf: 'stretch', height: 300 }}
-                showinfo={true}
-                onReady={res => {
-                    refVideo.current.seekTo(props.route.params.data.currentPosition)
-                    this.currentTime = props.route.params.data.currentPosition
-                    if (props.route.params.data.status == "playing") {
-                        setPlaying(true)
-                    } else {
-                        setPlaying(false)
-                    }
-                }}
-                controls={role == "Host" ? 1 : 0}
-                modestbranding={true}
-                play={isPlaying}
-                onChangeState={e => {
-                    this.videoStatus = e["state"]
-                }}
-                onError={e => console.log(e)}
-            />
+            <View
+                onLayout={r => setDime(r.nativeEvent.layout)}
+            >
+                {/* <View style={{ position: "absolute", top: 0, height: Math.round(dime.height), right: 20, width: Math.round(dime.width) / 3, zIndex: 20 }}>
+                    <Animated.Image
+                        source={require("../../assests/image/heaty.png")}
+                        resizeMode={"contain"}
+                        style={{ position: "absolute", bottom: bottomValue, height: (Math.round(dime.height) / 3) - 20, width: (Math.round(dime.width) / 3) - 20 }}
+                    />
+                </View> */}
+                <YouTube
+                    style={{ zIndex: 0 }}
+                    ref={refVideo}
+                    apiKey="AIzaSyCm7cvQdOwCnslbRqECA015md9Pj_n4ZnM"
+                    videoId={props.route.params.data["videoUrl"]}
+                    style={{ alignSelf: 'stretch', height: 300 }}
+                    showinfo={true}
+                    onReady={res => {
+                        refVideo.current.seekTo(props.route.params.data.currentPosition)
+                        this.currentTime = props.route.params.data.currentPosition
+                        if (props.route.params.data.status == "playing") {
+                            setPlaying(true)
+                        } else {
+                            setPlaying(false)
+                        }
+                    }}
+                    controls={role == "Host" ? 1 : 0}
+                    modestbranding={true}
+                    play={isPlaying}
+                    onChangeState={e => {
+                        this.videoStatus = e["state"]
+                    }}
+                    onChangeFullscreen={e => {
+                        this.isFullScreen = e.isFullscreen
+                    }}
+                    onError={e => console.log(e)}
+                />
+            </View>
 
             <View style={{ backgroundColor: "blue" }}>
                 <Text>a</Text>
             </View>
 
-        </View>
+        </View >
     );
 }
--- src/Store/Keys.js
+++ src/Store/Keys.js
@@ -1,3 +1,3 @@
 export const user = "UserProfile";
 export const name = "name";
-export const baseUrl = "http://192.168.43.249:4000"
+export const baseUrl = "http://192.168.225.101:4000"
